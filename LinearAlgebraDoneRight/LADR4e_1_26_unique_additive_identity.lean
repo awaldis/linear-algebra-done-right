c8904a428a25690ea3bbfb4925418776b4abecb6
Andrew Waldis <andrew@andrewwaldis.com>
2025-09-30 11:49:37 -0400

    Add proof of theorem 1.26 unique additive identity

---
+++ LinearAlgebraDoneRight/LADR4e_1_26_unique_additive_identity.lean
@@ -0,0 +1,21 @@
+------------------------------------------------------
+-- 1.26 - unique additive identity
+-- Follows the proof in the book step-by-step
+------------------------------------------------------
+import Mathlib.Algebra.Module.Basic
+import Mathlib.Algebra.Field.Basic
+
+variable {𝔽 : Type*} [Field 𝔽]
+variable {V : Type*} [AddCommGroup V] [Module 𝔽 V]
+variable {zero' : V} -- candidate second additive identity
+
+example
+  -- zero' is an addditive identity
+  (h_zero'_add_id : ∀ v : V, v + zero' = v)
+
+  -- Show that zero' and the original zero are identical.
+  : zero' = (0 : V) := by
+  calc zero'
+      = zero' + 0 := by rw [add_zero]
+    _ = 0 + zero' := by rw [add_comm]
+    _ = (0 : V)   := by simpa using h_zero'_add_id
