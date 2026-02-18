8f8c014118ac93a6b02a668ed12f01510b973fc4
Andrew Waldis <andrew@andrewwaldis.com>
2026-02-18 17:17:17 -0500

    Add better comments to theorem 1.30

--- LinearAlgebraDoneRight/LADR4e_1_30_the_number_0_times_a_vector.lean
+++ LinearAlgebraDoneRight/LADR4e_1_30_the_number_0_times_a_vector.lean
@@ -1,15 +1,21 @@
--------------------------------------------------------------
--- 1.30 - the number zero times any vector is the zero vector
--------------------------------------------------------------
 import Mathlib.Algebra.Module.Basic
 import Mathlib.Algebra.Field.Basic
-
+/-!
+# Theorem 1.30 - The number zero times any vector is the zero vector.
+0𝑣 = 0 for every 𝑣 ∈ 𝑉
+## From:
+Sheldon Axler. [Linear Algebra Done Right](https://linear.axler.net), fourth
+edition, Undergraduate Texts in Mathematics, Springer, 2024
+-/
 variable {𝔽 : Type*} [Field 𝔽]
 variable {V : Type*} [AddCommGroup V] [Module 𝔽 V]
 variable {v : V}
 
-example
-  : (0:𝔽) • v = (0:V) := by
+-- Key step - "add_smul" (scalar multiplication distributes over scalar
+-- addition) turns scalar multiplication to vector addition.  From there
+-- we show one vector is the additive inverse of the other.
+
+theorem scalar_0_times_any_vector_is_0 : (0:𝔽) • v = (0:V) := by
   calc (0:𝔽) • (v : V)
       = ((0:𝔽) + (0:𝔽)) • v                    := by rw[add_zero]
     _ = (0:𝔽) • v + (0:𝔽) • v                  := by rw[add_smul]
