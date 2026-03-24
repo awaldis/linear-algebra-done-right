f4b84dc4b07a7f4e51304cede890723d149e1a99
Andrew Waldis <andrew@andrewwaldis.com>
2026-03-24 09:46:25 -0400

    Add additive inverse proof to exercise 1C-7

--- LinearAlgebraDoneRight/LADR4e_3_Exercise_1C-7.lean
+++ LinearAlgebraDoneRight/LADR4e_3_Exercise_1C-7.lean
@@ -15,9 +15,13 @@ edition, Undergraduate Texts in Mathematics, Springer, 2024
 -/
 
 -------------------------------------------------------------------------------
--- Our "U" will be a subset of ℝ² that provides a counterexample - namely, all
--- the realnumbers that are also integers.
-
+-- Our "U" will provide a counterexample that specifies a subset of ℝ² that
+-- possesses the closure under addition and additive inverse properties but
+-- fails closure under scalar multiplication.
+--
+-- In summary, the counterexample set will be all the vectors whose elements
+-- consist only of real numbers that can be cast to integers.
+--
 -- Specifically, when every vector in ℝ² (Fin 2 → ℝ) is applied to every index
 -- in Fin 2, the vector is considered a member of the subset only if both real
 -- numbers produced are equivalent to some integer n coerced to a real number.
@@ -53,10 +57,31 @@ theorem intPairs_add_closed {vec1 vec2 : Fin 2 → ℝ}
   rw [h_cast]
 
 -----------------------------------------------------------------------------
--- Prove that the additive inverse of every element of "intPairsSubset" is
--- also in "intPairsSubset".
+-- Prove that the additive inverse of every element of 'intPairsSubset' is
+-- also in 'intPairsSubset'.
 -----------------------------------------------------------------------------
--- TBD
+theorem intPairs_add_inv_exists {vec : Fin 2 → ℝ}
+                                (h_vec_in_subset : vec ∈ intPairsSubset) :
+                                -vec ∈ intPairsSubset := by
+  unfold intPairsSubset at *
+  simp only [Set.mem_setOf_eq] at *
+
+  -- Goal: ∀ (i : Fin 2), ∃ n, (-vec) i = ↑n
+
+  -- Move index from goal to context.
+  intro (i:Fin 2)
+
+  -- Unpack input hypotheses.
+  obtain ⟨int, h_vec_i_eq_int⟩ := h_vec_in_subset i
+
+  use -int
+  simp only [Pi.neg_apply]
+  simp only [h_vec_i_eq_int]
+
+  -- Prove the cast can be done either before or after negation.
+  have h_cast : (↑(-(int: ℤ)) : ℝ) = -(↑(int:ℤ):ℝ) := Int.cast_neg int
+  rw [h_cast]
+
 
 -----------------------------------------------------------------------------
 -- NOT closed under scalar multiplication.
