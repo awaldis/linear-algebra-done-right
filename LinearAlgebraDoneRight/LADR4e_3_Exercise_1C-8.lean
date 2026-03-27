60a5eb0dfa2f9654dc6d34f7ca53e3eecc010bab
Andrew Waldis <andrew@andrewwaldis.com>
2026-03-27 10:21:37 -0400

    Add the completed version of exercise 1C-8.

--- LinearAlgebraDoneRight/LADR4e_3_Exercise_1C-8.lean
+++ LinearAlgebraDoneRight/LADR4e_3_Exercise_1C-8.lean
@@ -51,3 +51,67 @@ theorem smul_closed (c:ℝ) {vec : Fin 2 → ℝ}
     rw [h_on_y_axis]
     rw [smul_eq_mul]
     ring
+
+-----------------------------------------------------------------------------
+-- Show that if we assume the the set is a subspace then vector addition
+-- can result in an element that is NOT in the set.
+-----------------------------------------------------------------------------
+theorem unionSubset_not_subspace :
+  ¬ ∃ (S : Submodule ℝ  (Fin 2 → ℝ)), (S :Set (Fin 2 → ℝ)) =
+                                                 unionSubset := by
+  intro ⟨S, hS⟩
+
+  ----------------------------------------------------------------------------
+  -- Prove (1,1) IS NOT in the set (that is supposedly a subspace) according
+  -- to the set builder definition.
+  ----------------------------------------------------------------------------
+  have h_1_1_not_in_set : ¬![1, 1] ∈ unionSubset := by
+    unfold unionSubset at *
+    simp only [Set.mem_union] at *
+
+    -- Change NOR to AND
+    push_neg
+    constructor
+    · intro h_in_x_Subset
+      unfold x_Subset at *;  simp only [Set.mem_setOf_eq] at *
+      simp [Matrix.cons_val_one] at h_in_x_Subset
+    · intro h_in_y_Subset
+      unfold y_Subset at *;  simp only [Set.mem_setOf_eq] at *
+      simp [Matrix.cons_val_zero] at h_in_y_Subset
+
+  ----------------------------------------------------------------------------
+  -- Prove (1,1) IS in the set (that is supposedly a subspace) if we use the
+  -- closure rules of subspaces.
+  ----------------------------------------------------------------------------
+  have h_1_1_in_set : ![1, 1] ∈ unionSubset := by
+
+    have h_1_0_in_subspace : ![1, 0] ∈ S := by
+      have : ![1, 0] ∈ unionSubset := by
+        unfold unionSubset; left; unfold x_Subset; simp only [Set.mem_setOf_eq]
+        simp
+      rw [← hS] at this; rwa [SetLike.mem_coe] at this
+
+    have h_0_1_in_subspace : ![0, 1] ∈ S := by
+      have : ![0, 1] ∈ unionSubset := by
+        unfold unionSubset; right; unfold y_Subset; simp only [Set.mem_setOf_eq]
+        simp
+      rw [← hS] at this; rwa [SetLike.mem_coe] at this
+
+    have h_1_1_in_subspace : ![1, 1] ∈ S := by
+
+      -- This is the crux of the proof.  Adding two elements in the subspace
+      -- can result in an element that is outside the set definition.
+      have h_sum_is_member: ![1, 0] + ![0, 1] ∈ S :=
+                         S.add_mem h_1_0_in_subspace h_0_1_in_subspace
+
+      have h_sum_eq_1_1 : ![(1:ℝ), 0] + ![0, 1] = ![1, 1] := by
+        ext i
+        fin_cases i <;> simp
+
+      rwa [h_sum_eq_1_1] at h_sum_is_member
+
+    rw [← hS]; rw [SetLike.mem_coe]
+    exact h_1_1_in_subspace
+
+  -- Assuming S is a subspace leads to a contradiction.
+  exact absurd h_1_1_in_set h_1_1_not_in_set
