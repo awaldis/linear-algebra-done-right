08b7be6e1a4db77efdde819cbf9a85bce9b7bddc
Andrew Waldis <andrew@andrewwaldis.com>
2026-04-01 11:51:55 -0400

    Add the completed version of exercise 1C-17.

--- LinearAlgebraDoneRight/LADR4e_3_Exercise_1C-17.lean
+++ LinearAlgebraDoneRight/LADR4e_3_Exercise_1C-17.lean
@@ -16,11 +16,8 @@ edition, Undergraduate Texts in Mathematics, Springer, 2024
 variable {𝔽 : Type*} [Field 𝔽]
 variable {V : Type*} [AddCommGroup V] [Module 𝔽 V]
 -------------------------------------------------------------------------------
--- Define what it means to be in the set of the sum of two subspaces.
+-- Define what it means to be the sum of two subspaces.
 -------------------------------------------------------------------------------
-def sum2_SubspacesSet ( U W : Submodule 𝔽 V ) : Set V :=
-  {x | ∃ u w : V, u ∈ U ∧ w ∈ W ∧ x = u + w}
-
 def sum2_Subspaces (U W : Submodule 𝔽 V) : Submodule 𝔽 V where
   carrier := {x | ∃ u w : V, u ∈ U ∧ w ∈ W ∧ x = u + w}
   zero_mem' := by
@@ -39,7 +36,6 @@ def sum2_Subspaces (U W : Submodule 𝔽 V) : Submodule 𝔽 V where
       · rw [h_sum_u₁_w₁, h_sum_u₂_w₂]
         ac_rfl
   smul_mem' := by
-    -- Prove closure under scalar multiplication
     intro c v h_v_in_sum
     rcases h_v_in_sum with ⟨u, w, h_u_in_U, h_w_in_W, h_sum_u_w⟩
     use (c • u)
@@ -51,28 +47,48 @@ def sum2_Subspaces (U W : Submodule 𝔽 V) : Submodule 𝔽 V where
       · rw [h_sum_u_w]
         simp
 
-
 -----------------------------------------------------------------------------
 -- The answer to the exercise is yes and here is the proof.
 ------------------------------------------------------------------------------
-theorem sum_of_subspaces_commutative ( V₁ V₂ V₃ : Submodule 𝔽 V ) :
+theorem sum_of_subspaces_associative ( V₁ V₂ V₃ : Submodule 𝔽 V ) :
                           sum2_Subspaces (sum2_Subspaces V₁ V₂) V₃ =
                           sum2_Subspaces V₁ (sum2_Subspaces V₂ V₃) := by
-  --unfold sum2_Subspaces at *
   ext x
   constructor
-  · -- U + W → W + U
-    intro h_in_the_sum_of_subspaces
-    simp only [Set.mem_setOf_eq] at *
-    rcases h_in_the_sum_of_subspaces with ⟨u, w, h_u_in_U, h_w_in_W, h_sum_u_w⟩
-    use w, u
-    rw [h_sum_u_w, add_comm]
-    exact ⟨h_w_in_W, h_u_in_U, rfl ⟩
+  · -- (V₁ + V₂) + V₃ → V₁ + (V₂ + V₃)
+    intro h_in_the_sum_of_sumV₁V₂_V₃
+    rcases h_in_the_sum_of_sumV₁V₂_V₃ with ⟨sum_v₁_v₂, v₃,
+                                            h_sum_v₁_v₂_in_sumset_V₁_V₂,
+                                            h_v₃_in_V₃, h_sum_v₁v₂_v₃⟩
+    rcases h_sum_v₁_v₂_in_sumset_V₁_V₂ with ⟨v₁, v₂,
+                                            h_v₁_in_V₁,
+                                            h_v₂_in_V₂, h_sum_v₁_v₂⟩
+    rw[h_sum_v₁_v₂] at h_sum_v₁v₂_v₃
+    rw [h_sum_v₁v₂_v₃]
 
-  · -- W + U → U + W
-    intro h_in_the_sum_of_subspaces
-    simp only [Set.mem_setOf_eq] at *
-    rcases h_in_the_sum_of_subspaces with ⟨w, u, h_w_in_W, h_u_in_U, h_sum_w_u⟩
-    use u, w
-    rw [h_sum_w_u, add_comm]
-    exact ⟨h_u_in_U, h_w_in_W, rfl ⟩
+    use v₁, v₂ + v₃
+
+    constructor
+    · exact h_v₁_in_V₁
+    · constructor
+      · use v₂, v₃
+      · ac_rfl
+
+  · -- V₁ + (V₂ + V₃) → (V₁ + V₂) + V₃
+    intro h_in_the_sum_of_sumV₁_V₂V₃
+    rcases h_in_the_sum_of_sumV₁_V₂V₃ with ⟨v₁, sum_v₂_v₃, h_v₁_in_V₁,
+                                            h_sum_v₂_v₃_in_sumset_V₂_V₃,
+                                            h_sum_v₁_v₂v₃⟩
+    rcases h_sum_v₂_v₃_in_sumset_V₂_V₃ with ⟨v₂, v₃,
+                                            h_v₂_in_V₂,
+                                            h_v₃_in_V₃, h_sum_v₂_v₃⟩
+    rw[h_sum_v₂_v₃] at h_sum_v₁_v₂v₃
+    rw [h_sum_v₁_v₂v₃]
+
+    use v₁ + v₂, v₃
+
+    constructor
+    · use v₁, v₂
+    · constructor
+      · exact h_v₃_in_V₃
+      · ac_rfl
