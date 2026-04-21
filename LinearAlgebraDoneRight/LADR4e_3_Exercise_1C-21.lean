d2863f31b3cf2327b373a43841d908d41cf160fd
Andrew Waldis <andrew@andrewwaldis.com>
2026-04-21 14:19:27 -0400

    Add the final version of exercise 1C-21.

---
+++ LinearAlgebraDoneRight/LADR4e_3_Exercise_1C-21.lean
@@ -0,0 +1,196 @@
+import Mathlib.Algebra.Field.Basic
+import Mathlib.Algebra.Module.Submodule.Basic
+import Mathlib.Tactic  -- Needed for "ring"
+
+set_option linter.style.emptyLine false
+set_option linter.style.whitespace false
+
+/-!
+# Suppose 𝑈 = {(𝑥, 𝑦, 𝑥 + 𝑦, 𝑥 − 𝑦, 2𝑥) ∈ 𝐅⁵ ∶ 𝑥, 𝑦 ∈ 𝐅}.
+# Find a subspace 𝑊 of 𝐅⁵ such that 𝐅⁵ = 𝑈 ⊕ 𝑊.
+## From:
+Sheldon Axler. [Linear Algebra Done Right](https://linear.axler.net), fourth
+edition, Undergraduate Texts in Mathematics, Springer, 2024
+
+The code will show that W = {(0, x, 0, y) : x, y ∈ 𝔽} is a subspace that meets
+criteria.
+-/
+variable {𝔽 : Type*} [Field 𝔽]
+
+-- ═══════════════════════════════════════════════════════════════════════════
+-- Define U = {(x, y, x + y, x - y, 2x) : x, y ∈ 𝔽} as a subspace of 𝔽⁵.
+-- ═══════════════════════════════════════════════════════════════════════════
+def U : Submodule 𝔽 (Fin 5 → 𝔽) where
+  carrier := { v : Fin 5 → 𝔽 |
+                     (v 2 = v 0 + v 1) ∧ (v 3 = v 0 - v 1) ∧ (v 4 = 2 * v 0) }
+
+  zero_mem' := by
+    simp only [Set.mem_setOf_eq]
+    simp only [Pi.zero_apply]
+    ring_nf
+    tauto
+
+  -- Show that if a and b are both in U, then a + b is in U.
+  add_mem' := by
+    intro a b h_a_in_set h_b_in_set
+    simp only [Set.mem_setOf_eq, Pi.add_apply] at *
+    -- h_a_in_set : a 0 = a 1 ∧ a 2 = a 3
+    -- h_b_in_set : b 0 = b 1 ∧ b 2 = b 3
+    obtain ⟨h_a2_eq_suma0a1, h_a3_eq_diffa0a1, h_a4_eq_2a0⟩ := h_a_in_set
+    obtain ⟨h_b2_eq_sumb0b1, h_b3_eq_diffb0b1, h_b4_eq_2b0⟩ := h_b_in_set
+    refine ⟨ ?_, ?_, ?_⟩
+    · -- Goal: a 2 + b 2 = a 0 + b 0 + (a 1 + b 1)
+      rw [h_a2_eq_suma0a1, h_b2_eq_sumb0b1]
+      ring
+    · -- Goal: a 3 + b 3 = a 0 + b 0 - (a 1 + b 1)
+      rw [h_a3_eq_diffa0a1, h_b3_eq_diffb0b1]
+      ring
+    · -- Goal: a 4 + b 4 = 2 * (a 0 + b 0)
+      rw [h_a4_eq_2a0, h_b4_eq_2b0]
+      ring
+
+  -- Show that if u is in U and c ∈ 𝔽, then c • u is in U.
+  smul_mem' := by
+    intro c u h_u_in_set
+    simp only [Set.mem_setOf_eq, Pi.smul_apply, smul_eq_mul] at *
+    obtain ⟨h_u2_eq_sumu0u1, h_u3_eq_diffu0u1, h_u4_eq_2u0⟩ := h_u_in_set
+    refine ⟨ ?_, ?_, ?_⟩
+    · -- Goal: c * u 2 = c * u 0 + c * u 1
+      rw [h_u2_eq_sumu0u1]
+      ring
+    · -- Goal: c * u 3 = c * u 0 - c * u 1
+      rw [h_u3_eq_diffu0u1]
+      ring
+    · -- Goal: c * u 4 = 2 * (c * u 0)
+      rw [h_u4_eq_2u0]
+      ring
+
+-- ═══════════════════════════════════════════════════════════════════════════
+-- Define W = {(0, 0, x, y, z) : x, y, z ∈ 𝔽} as a subspace of 𝔽⁵.
+-- ═══════════════════════════════════════════════════════════════════════════
+def W : Submodule 𝔽 (Fin 5 → 𝔽) where
+  carrier := { v : Fin 5 → 𝔽 | (v 0 = 0) ∧ (v 1 = 0) }
+
+  zero_mem' := by
+    simp only [Set.mem_setOf_eq]
+    simp only [Pi.zero_apply]
+    tauto
+
+  -- Show that if a and b are both in W, then a + b is in W.
+  -- Specifically, (a + b) 0 = 0 and
+  --               (a + b) 1 = 0
+  add_mem' := by
+    intro a b h_a_in_set h_b_in_set
+    simp only [Set.mem_setOf_eq, Pi.add_apply] at *
+
+    obtain ⟨h_a0_eq_0, h_a1_eq_0⟩ := (h_a_in_set : a 0 = 0 ∧ a 1 = 0)
+    obtain ⟨h_b0_eq_0, h_b1_eq_0⟩ := (h_b_in_set : b 0 = 0 ∧ b 1 = 0)
+    constructor
+    · -- Goal: a 0 + b 0 = 0
+      rw [h_a0_eq_0, h_b0_eq_0, zero_add]
+    · -- Goal: a 1 + b 1 = 0
+      rw [h_a1_eq_0, h_b1_eq_0, zero_add]
+
+  -- Show that if w is in W and c ∈ 𝔽, then c • w is in W.
+  smul_mem' := by
+    intro c w h_w_in_set
+    simp only [Set.mem_setOf_eq, Pi.smul_apply, smul_eq_mul] at *
+    obtain ⟨h_w0_eq_0, h_w1_eq_0⟩ := h_w_in_set
+    constructor
+    · -- Goal: c * w 0 = 0
+      rw [h_w0_eq_0, mul_zero]
+    · -- Goal: c * w 1 = 0
+      rw [h_w1_eq_0, mul_zero]
+
+-- ═══════════════════════════════════════════════════════════════════════════
+-- Show that subspaces U and W intersect only at zero.  Therefore, their sum
+-- is a direct sum by theorem 1.46.
+-- ═══════════════════════════════════════════════════════════════════════════
+theorem U_intersect_W_eq_zero :
+  ∀ v : Fin 5 → 𝔽, (v ∈ U) ∧ (v ∈ W) → (v = 0) := by
+
+  intro v ⟨h_v_in_U, h_v_in_W⟩
+
+  unfold U at h_v_in_U
+  unfold W at h_v_in_W
+
+  obtain ⟨h_v2_eq_sumv0v1, h_v3_eq_diffv0v1, h_v4_eq_2v0⟩ := h_v_in_U
+  obtain ⟨h_v0_eq_zero,    h_v1_eq_zero⟩                  := h_v_in_W
+
+  have h_v2_eq_zero : v 2 = 0 := by rw [h_v2_eq_sumv0v1, h_v0_eq_zero,
+                                                         h_v1_eq_zero ]
+                                    ring
+
+  have h_v3_eq_zero : v 3 = 0 := by rw [h_v3_eq_diffv0v1, h_v0_eq_zero,
+                                                         h_v1_eq_zero ]
+                                    ring
+
+  have h_v4_eq_zero : v 4 = 0 := by rw [h_v4_eq_2v0, h_v0_eq_zero]
+                                    ring
+
+
+  ext i
+  simp only [Pi.zero_apply]
+  fin_cases i <;> assumption
+
+-- ═══════════════════════════════════════════════════════════════════════════
+-- Show that the sum of subspaces U and W equal all of 𝔽⁵.  This is not
+-- necessary to prove it's a direct sum, but is a requirement of the problem
+-- statement.
+-- ═══════════════════════════════════════════════════════════════════════════
+theorem sum_U_W_eq_F5 :
+  ∀ (v : Fin 5 → 𝔽), ∃ (u w : Fin 5 → 𝔽), (u ∈ U) ∧ (w ∈ W) ∧ (v = u + w) := by
+
+  intro v
+
+  -- Define our proposed members of U and W that will supposedly meet the
+  -- criteria of the theorem.
+  let u : Fin 5 → 𝔽 := fun i => match i with
+    | 0 => v 0
+    | 1 => v 1
+    | 2 => v 0 + v 1
+    | 3 => v 0 - v 1
+    | 4 => 2 * v 0
+
+  let w : Fin 5 → 𝔽 := fun i => match i with
+    | 0 => 0
+    | 1 => 0
+    | 2 => v 2 - (v 0 + v 1)
+    | 3 => v 3 - (v 0 - v 1)
+    | 4 => v 4 - (2 * v 0)
+
+  use u, w
+
+  -- Break out the three AND clauses in the goal into three separate goals.
+  refine ⟨?_, ?_, ?_⟩
+
+  · -- Goal 1: u ∈ U
+    unfold U
+    refine ⟨ ?_, ?_, ?_ ⟩
+    · rfl -- u 2 = u 0 + u 1
+    · rfl -- u 3 = u 0 - u 1
+    · rfl -- u 4 = 2 * u 0
+
+  · -- Goal 2: w ∈ W
+    unfold W
+    constructor
+    · rfl -- w 0 = 0
+    · rfl -- w 1 = 0
+
+  · -- Goal 3: v = u + w
+    ext i
+    fin_cases i
+    · calc v 0 = v 0 + 0   := by ring
+           _   = u 0 + w 0 := by simp [u, w]
+
+    · calc v 1 = v 1 + 0    := by ring
+           _   = u 1 + w 1  := by simp [u, w]
+
+    · calc v 2 = (v 0 + v 1) + ( v 2 - (v 0 + v 1))  := by ring
+           _   = u 2 + w 2                           := by simp [u, w]
+
+    · calc v 3 = (v 0 - v 1) + ( v 3 - (v 0 - v 1))  := by ring
+           _   = u 3 + w 3                           := by simp [u, w]
+
+    · calc v 4 = (2 * v 0) + (v 4 - (2 * v 0)) := by ring
+           _   = u 4 + w 4                     := by simp [u, w]
